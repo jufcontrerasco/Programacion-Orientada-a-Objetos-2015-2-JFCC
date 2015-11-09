b8cb333c78de3c8cdcf8da9dacb469ce65785620
Juan Felipe Contreras Correa <jufcontrerasco@unal.edu.co>
2015-11-08 23:57:21 -0500

    Taller 4 - Juan Felipe Contreras

--- Taller 4/BanquitasUN - Punto 5/src/Banquitas/Equipos.java
+++ Taller 4/BanquitasUN - Punto 5/src/Banquitas/Equipos.java
@@ -1,22 +1,13 @@
- 	package Banquitas;
+package Banquitas;
 
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Equipos {
 	
-<<<<<<< HEAD
 	String nombre;
 	int gol = 0;
 	ArrayList<Jugadores> equipo = new ArrayList<>();
-=======
-	Scanner sc = new Scanner(System.in);
-	public String nombre;
-	int gol = 0;
-	Jugadores[] equipo = new Jugadores[5];
-	int goles;
-	int puntos = 0;
->>>>>>> origin/master
 	
 	public void crearEquipo(){
 		Scanner sc = new Scanner(System.in);
@@ -25,42 +16,12 @@ public class Equipos {
 		System.out.println("Listo!, este es el nombre de tu equipo: "+equi);		
 	}
 	
-	public void editarEquipo(){
+	public void agregarEquipo(){
 		
-		System.out.println("Editar equipo:");
-		System.out.println("Seleccione el equipo a editar:");
+		System.out.println("Agregar equipo:");
+		System.out.println("Por favor ingrese la siguiente informacion: ");
 		
-<<<<<<< HEAD
 		for(int id=0; id<=4;id++){
-			int c = id+1;
-			Scanner sc = new Scanner(System.in);
-			Jugadores tmp = new Jugadores();
-			System.out.println("Por favor ingrese el nombre del jugador numero "+c+" :");
-			tmp.setNombre(sc.nextLine());
-			System.out.println("Por favor ingrese la posicion del jugador numero "+c+" :");
-			tmp.setPosicion(sc.nextLine());
-			System.out.println("Por favor ingrese la edad del jugador numero "+c+" :");
-			tmp.setEdad(sc.nextInt());
-			System.out.println("Por favor ingrese la dorsal del jugador numero "+c+" :");
-			tmp.setDorsal(sc.nextInt());
-			System.out.println("Por favor ingrese la habilidad del jugador numero "+c+" :");
-			tmp.setHabilidad(sc.nextLine());
-			tmp.setGoles(gol);
-			System.out.println("Estos son los datos del jugador numero "+c+" :");
-			tmp.mostrarJugador();
-			equipo.add(tmp);
-		}
-	}
-	
-	public void agregarJugadores(){
-=======
-		
-	}
-	
-public Jugadores[] agregarJugadores(){
->>>>>>> origin/master
-		System.out.println("Ingrese los datos de sus jugadores...");
-		for(int id=0; id<=5;id++){
 			int c = id+1;
 			Scanner sc = new Scanner(System.in);
 			Jugadores tmp = new Jugadores();
@@ -76,28 +37,20 @@ public Jugadores[] agregarJugadores(){
 			tmp.setHabilidad(sc.next());
 			tmp.setGoles(gol);
 			System.out.println("Estos son los datos del jugador numero "+c+" :");
-			tmp.mostrarJugador();							
+			tmp.mostrarJugador();
+			equipo.add(tmp);
+		}
+	}
+	
+	public void agregarJugadores(){
+							
 		}
 		
-		System.out.println("Su nuevo equipo ahora es: ");
-		mostrarEquipo();
-	}
-<<<<<<< HEAD
+	
 	
 	
 	
 	public void mostrarEquipo(){
-=======
-
-public int PartidoGanado(){
-	puntos += 3;
-}
-public int PartidoEmpatado(){
-	puntos += 1;
-}
-
-public Jugadores[] mostrar_equipo(){
->>>>>>> origin/master
 		System.out.println("Los jugadores de este equipo son: ");
 		Jugadores tmp = new Jugadores();
 		for(int id=0; id<=4;id++){			
@@ -105,8 +58,11 @@ public Jugadores[] mostrar_equipo(){
 		}
 	}
 	
-	public Equipos(){
-
+	public Equipos(String nombre, ArrayList<Jugadores> equipo){
+			this.nombre = nombre;
+			this.equipo = equipo;
 	}
+	public Equipos(){
+}
 	
 }
--- Taller 4/BanquitasUN - Punto 5/src/Banquitas/Main.java
+++ Taller 4/BanquitasUN - Punto 5/src/Banquitas/Main.java
@@ -1,6 +1,12 @@
 package Banquitas;
 
 
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -16,7 +22,7 @@ public class Main {
 		
 		Scanner menu = new Scanner(System.in);	
 		int men = menu.nextInt();
-		Equipos jugadores = new Equipos();		
+		ArrayList<Equipos> Final = new ArrayList<>();
 		
 		switch(men){
 		
@@ -30,8 +36,10 @@ public class Main {
 			while(Select < 1 || Select > 3 );
 			switch(Select){
 			case 1:
-				jugadores.crearEquipo();
-				jugadores.agregarJugadores();
+				Equipos Equipo = new Equipos();	
+				Equipo.crearEquipo();
+				Equipo.agregarEquipo();
+				Final.add(Equipo);
 				break;
 
 			case 2:
@@ -59,9 +67,88 @@ public class Main {
 		case 4:
 			
 			break;
+		default:
+			System.out.println("Numero invalido, ingrese otro numero porfavor...");
+			break;
 		}
 		
+		 imprimir(Final);
 		
 	}
+	
+	public static void imprimir(ArrayList<Equipos> fin){
+		try {
+			FileOutputStream file = new FileOutputStream("jugadores.txt",true);
+			PrintWriter p = new PrintWriter(file);
+			
+			p.println(fin.size());
+			
+			for(int i = 0; i < fin.size(); i++) {
+				Equipos auxEquipo = fin.get(i);
+				p.println(auxEquipo.nombre); /// Array List de jugadores
+				p.println(auxEquipo.equipo.size()); /// Array List de jugadores
+				for(int j = 0; j < auxEquipo.equipo.size(); j++) {
+					Jugadores aux = auxEquipo.equipo.get(j);
+					p.println(aux.nombre + " " + aux.posicion + " "+ aux.edad + " "+ aux.dorsal+ " "+ aux.goles+ " "+aux.habilidad);
+				}
+			}
+			
+			p.close();
+			file.close();
+			
+		} catch (IOException e) {
+			System.out.println("Hubo un problema escribiendo en el archivo");
+		}
+	}
+	
+	
+	/*public ArrayList<Equipos> Leer (String nombre, ArrayList<Jugadores> equipo){
+		try {
+        	FileReader file = new FileReader("jugadores.txt");
+        	BufferedReader reader = new BufferedReader(file);
+
+        	String line;
+    
+        	while((line = reader.readLine()) != null) {
+        		line = reader.readLine();
+        		if(line == null) break;
+
+        		String all[] = line.split(" ");
+        		String name = all[0];
+        		String posicion = all[1];
+        		int edad = Integer.parseInt(all[2]);
+        		int dorsal = Integer.parseInt(all[3]);
+        		int goles = Integer.parseInt(all[4]);
+        		String habilidad = all[5];
+        
+        		
+        		Jugadores aux = new Jugadores();
+        		aux.setNombre(nombre);
+        		aux.setPosicion(posicion);
+        		aux.setEdad(edad);
+        		aux.setDorsal(dorsal);
+        		aux.setGoles(goles);
+        		aux.setHabilidad(habilidad);
+        		
+        		
+        	Equipos.add(aux);
+        	}
+        	
+        	reader.close();
+        	file.close();
+        	
+        } catch(IOException e) {
+         e.printStackTrace();
+        	System.out.println("Hubo un problema leyendo");
+        }
+    	
+        for(int i = 0; i < Jugadores.size(); i++) {
+        	System.out.println(personas.get(i).id + " - " + personas.get(i).nombre);
+       }
+    
+		return null;
+		
+	}*/
+
 
 }
